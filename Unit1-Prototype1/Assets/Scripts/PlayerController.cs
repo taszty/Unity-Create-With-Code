e7596fbbf92f57bcf9c6ef43b0449f6b349305b0
nelson-dylan <36965721+taszty@users.noreply.github.com>
2024-01-08 13:36:51 -0700

    Updated 1.4

--- Unit1-Prototype1/Assets/Scripts/PlayerController.cs
+++ Unit1-Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-
-
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     // variables
     public float speed = 20f;
+    public float turnSpeed = 45f;
+    public float horInput;
+    public float forInput;
 
     // Update is called once per frame
     void Update()
     {
-        // move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // horizontal/vertical input
+        horInput = Input.GetAxis("Horizontal");
+        forInput = Input.GetAxis("Vertical");
+        // move forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forInput);
+        // rotate based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horInput * Time.deltaTime);
     }
 }
